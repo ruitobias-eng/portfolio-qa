edae94e1671a2544354de94ebeca8f93ebc83ad4
ruitobias-eng <ruitobiascarvalho@hotmail.com>
2025-09-22 18:27:06 -0300

    primeiro commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: "/portfolio-qa/", // 👈 importante para GitHub Pages
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
